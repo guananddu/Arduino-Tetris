11ae9f4314b6d8a2d11d5b6d23b2ff667cd3ca48
Pydronia <jackscmail@gmail.com>
2014-04-04 16:35:07 +1100

    Update O_Tetromino.h

---
+++ Tetromino/O_Tetromino.h
@@ -0,0 +1,41 @@
+// O-shaped tetromino
+
+#ifndef TETROMINO_O_TETROMINO // prevents errors from including it multiple times
+#define TETROMINO_O_TETROMINO
+
+#include "Tetromino.h"
+
+class O_Tetromino : protected Tetromino{
+
+    ///////////////////////// PUBLIC ATTRIBUTES ////////////////////////////////
+    public:
+
+        char blockLetter(){
+            return 'O';
+        }
+
+    ///////////////////////// PROTECTED ATTRIBUTES /////////////////////////////
+    protected:
+
+        int** getShape() {
+            return shape;
+        }
+
+    ///////////////////////// PRIVATE ATTRIBUTES ///////////////////////////////
+    private:
+
+        int i0[4][2] = {{0,0},{0,1},{1,0},{1,1}}; // top piece -= 1 for 0 and -=22 for 2
+        int** shape;
+        int offset;
+
+
+    ///////////////////////// CONSTRUCTOR //////////////////////////////////////
+    O_Tetromino(){
+        startPosition = 3;
+        shape = (int**) i0;
+        offset = 0;
+    }
+
+};
+
+#endif
