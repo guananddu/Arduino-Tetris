25a9cdc4229bf7e01db41ec3db8cd77bb40eb884
Pydronia <jackscmail@gmail.com>
2014-04-04 16:45:19 +1100

    Update Z_Tetromino.h

---
+++ Tetromino/Z_Tetromino.h
@@ -0,0 +1,41 @@
+// T-shaped tetromino
+
+#ifndef TETROMINO_T_TETROMINO // prevents errors from including it multiple times
+#define TETROMINO_T_TETROMINO
+
+#include "Tetromino.h"
+
+class T_Tetromino : protected Tetromino{
+
+    ///////////////////////// PUBLIC ATTRIBUTES ////////////////////////////////
+    public:
+
+        char blockLetter(){
+            return 'T';
+        }
+
+    ///////////////////////// PROTECTED ATTRIBUTES /////////////////////////////
+    protected:
+
+        int** getShape() {
+            return shape;
+        }
+
+    ///////////////////////// PRIVATE ATTRIBUTES ///////////////////////////////
+    private:
+
+        int i0[4][2] = {{0,0},{1,0},{2,0},{1,1}}; // top piece -= 1 for 0 and -=22 for 2
+        int** shape;
+        int offset;
+
+
+    ///////////////////////// CONSTRUCTOR //////////////////////////////////////
+    T_Tetromino(){
+        startPosition = 3;
+        shape = (int**) i0;
+        offset = 0;
+    }
+
+};
+
+#endif
