fc566db0305381af51d701142e3283205a790f36
Pydronia <jackscmail@gmail.com>
2014-04-10 16:51:03 +1000

    Update S_Tetromino.h

--- Tetromino/S_Tetromino.h
+++ Tetromino/S_Tetromino.h
@@ -1,4 +1,4 @@
-// O-shaped tetromino
+// s-shaped tetromino
 // 2014 Christopher Claoue-Long
 
 #ifndef TETROMINO_O_TETROMINO // prevents errors from including it multiple times
@@ -6,13 +6,13 @@
 
 #include "Tetromino.h"
 
-class O_Tetromino : protected Tetromino{
+class S_Tetromino : protected Tetromino{
 
     ///////////////////////// PUBLIC ATTRIBUTES ////////////////////////////////
     public:
 
         char blockLetter(){
-            return 'O';
+            return 'S';
         }
 
     ///////////////////////// PROTECTED ATTRIBUTES /////////////////////////////
@@ -25,13 +25,13 @@ class O_Tetromino : protected Tetromino{
     ///////////////////////// PRIVATE ATTRIBUTES ///////////////////////////////
     private:
 
-        int i0[4][2] = {{0,0},{0,1},{1,0},{1,1}}; // top piece -= 1 for 0 and -=22 for 2
+        int i0[4][2] = {{1,0},{1,1},{0,1},{0,2}}; // top piece -= 1 for 0 and -=22 for 2
         int** shape;
         int offset;
 
 
     ///////////////////////// CONSTRUCTOR //////////////////////////////////////
-    O_Tetromino(){
+    S_Tetromino(){
         startPosition = 3;
         shape = (int**) i0;
         offset = 0;
