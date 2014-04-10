db3120432c23981aa259490877f289e72eff3369
Pydronia <jackscmail@gmail.com>
2014-04-10 17:22:47 +1000

    Updated MinoFactory
    Coding logic & control flow

--- MinoFactory.cpp
+++ MinoFactory.cpp
@@ -1,31 +1,30 @@
 // Tetromino Factory Class Functions
 // 2014 Abbie Wade
 
-#include "MinoFactory.h" 
+#include "MinoFactory.h"
+#include <vector>
+#include <algorithim>
 
 //  This method is called by controller to get a new piece when I need one.
 Tetromino MinoFactory::newPiece(){
     counter ++;
-    return shuffledPieces[counter-1];
-}
-
-//  This generates a new set of tetrominoes to pass to the controller when it is needed. This is
-//  where run newSet() and then shuffle the constant array.
-void MinoFactory::shuffleTetrominoes(){
-	for (/*i in generation*/){
-		//Put i into shuffled Pieces
+    if(counter > 7) { // finished up all the pieces!
+    	counter = 1;
+    	shuffleIntArray(); // now have a new generation of pieces
+    }
+    return generatedPieces[generation[counter-1]];
+    
+    }
 }
 
 void MinoFactory::shuffleIntArray()
-    //randomise "generation" array
-    
+    vector<int> randomiser(generation, generation+7);
+    random_shuffle(randomiser.begin(), randomiser.end()); 
+    int i = 0;
+    for (vector<int>::const_iterator it = randomiser.begin(); it != randomiser.end(); ++it ){
+    	generation[i] = it;
+    	i++;
+    }
+    }
 }
 
-//  This function resets the counter to 0 and copies the constant array to your shuffle array.
-void MinoFactory::newSet(){
-    if (counter >= 6){
-        shuffleTetrominoes();
-        counter = 0;
-    }
-	
-}
